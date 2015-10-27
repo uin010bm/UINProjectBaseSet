54f564baa9694e0a919e51c6042cec4759b0d9ad
Yu Tanaka <uin010bm@gmail.com>
2015-10-27 16:02:49 +0900

    Fix git ignore pods directory.

--- Pods/Target Support Files/Pods-UINProjectBaseSetTests/Pods-UINProjectBaseSetTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_UINProjectBaseSetTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_UINProjectBaseSetTestsVersionString[];
-
--- Pods/Target Support Files/Pods-UINProjectBaseSetUITests/Pods-UINProjectBaseSetUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UINProjectBaseSetUITests : NSObject
-@end
-@implementation PodsDummy_Pods_UINProjectBaseSetUITests
-@end
